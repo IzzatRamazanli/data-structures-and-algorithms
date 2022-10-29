4e4789337e9ff3610af0b9ed43117674122914dd
IzzatRamazanli <86556311+IzzatRamazanli@users.noreply.github.com>
2022-10-29 12:25:58 +0400

    factorial problem

--- src/main/java/com/izzat/recursion/FactorialProblem.java
+++ src/main/java/com/izzat/recursion/FactorialProblem.java
@@ -7,6 +7,7 @@ public class FactorialProblem {
     }
 
     public static long factorialRecursive(int num) {
+        if (num < 0) return -1;
         if (num == 0 || num == 1) return 1;
         else {
             return num * factorialRecursive(num - 1);
@@ -14,6 +15,7 @@ public class FactorialProblem {
     }
 
     public static long factorialIterative(int num) {
+        if (num < 0) return -1;
         long result = 1;
         for (int i = num; i > 0; i--) {
             result = result * i;
