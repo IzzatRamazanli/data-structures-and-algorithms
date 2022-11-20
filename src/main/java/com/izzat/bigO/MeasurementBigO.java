a50a576e7329dfd7692921b63f84d1786b5bc920
IzzatRamazanli <86556311+IzzatRamazanli@users.noreply.github.com>
2022-11-20 12:26:09 +0400

    big o measurement

---
+++ src/main/java/com/izzat/bigO/MeasurementBigO.java
@@ -0,0 +1,14 @@
+package com.izzat.bigO;
+
+public class MeasurementBigO {
+    //rule1 - any assigment statements and if statements that are executed once
+    //of the size of the problem - O(1)
+
+    //rule2 - a simple for loop from 0 to n (without internal loop) - O(n)
+
+    //rule3 - a nested loop - O(n^2)
+
+    //rule4 - a loop, in which the controlling parameter is divided by two at each step - O(logn)
+
+    //rule 5 - when dealing with multiple statements , just add them up
+}
--- src/main/java/com/izzat/recursion/practice/Main.java
+++ src/main/java/com/izzat/recursion/practice/Main.java
@@ -2,8 +2,10 @@ package com.izzat.recursion.practice;
 
 public class Main {
     public static void main(String[] args) {
-        int result = GreatestCommonDivisor.recursiveGCD(8, 12);
-        System.out.println(result);
+//        int result = GreatestCommonDivisor.recursiveGCD(8, 12);
+//        System.out.println(result);
+
+
 
     }
 }
