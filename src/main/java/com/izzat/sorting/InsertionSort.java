d9f9c0dcdd9b106584a0d518fb2db709f7795de0
IzzatRamazanli <86556311+IzzatRamazanli@users.noreply.github.com>
2022-11-29 22:55:36 +0400

    InsertionSort

--- src/main/java/com/izzat/sorting/InsertionSort.java
+++ src/main/java/com/izzat/sorting/InsertionSort.java
@@ -5,12 +5,11 @@ public class InsertionSort {
 
         for (int i = 1; i < arr.length; i++) {
             int temp = arr[i];
-            int index = i;
-            while (index > 0 && arr[index - 1] > temp) {
-                arr[index] = arr[index - 1];
-                index--;
+            while (i > 0 && arr[i - 1] > temp) {
+                arr[i] = arr[i - 1];
+                i--;
             }
-            arr[index] = temp;
+            arr[i] = temp;
         }
         return arr;
     }
