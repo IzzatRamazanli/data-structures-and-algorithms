7f81bbe3afe524da045eabd993c39a842ecf40cc
IzzatRamazanli <86556311+IzzatRamazanli@users.noreply.github.com>
2022-11-30 01:59:54 +0400

    BucketSort

--- src/main/java/com/izzat/sorting/SortMain.java
+++ src/main/java/com/izzat/sorting/SortMain.java
@@ -4,9 +4,10 @@ import java.util.Arrays;
 
 public class SortMain {
     public static void main(String[] args) {
-        int[] arr = {3, 5, 1, 2, 2, 6};
+        int[] arr = {3, 5, 1, 2, 2, 6, 9, 7, 8};
         //1 . int[] sortedArr = BubbleSort.bubbleSort(arr);
-        //int[] sortedArr = SelectionSort.selectionSort(arr);
+        //2 . int[] sortedArr = SelectionSort.selectionSort(arr);
+        //3 .int[] sortedArr = InsertionSort.insertionSort(arr);
         int[] sortedArr = InsertionSort.insertionSort(arr);
         System.out.println(Arrays.toString(sortedArr));
     }
