6a5d039a973a80e675b697572af946bfb1db4563
IzzatRamazanli <86556311+IzzatRamazanli@users.noreply.github.com>
2022-11-29 21:09:54 +0400

    SelectionSort

--- src/main/java/com/izzat/sorting/SortMain.java
+++ src/main/java/com/izzat/sorting/SortMain.java
@@ -5,7 +5,8 @@ import java.util.Arrays;
 public class SortMain {
     public static void main(String[] args) {
         int[] arr = {3, 5, 1, 2, 6};
-        int[] sortedArr = BubbleSort.bubbleSort(arr);
+        //1 . int[] sortedArr = BubbleSort.bubbleSort(arr);
+        int[] sortedArr = SelectionSort.selectionSort(arr);
         System.out.println(Arrays.toString(sortedArr));
     }
 }
